c4020a4a53ff578329be6b7d0213076a50d8d9fc
qwfy <qwfy5287@qq.com>
2025-07-04 16:05:32 +0800

    feat: env

--- frontend/mini-list-frontend/vite.config.ts
+++ frontend/mini-list-frontend/vite.config.ts
@@ -1,18 +1,31 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [
+      vue(),
+      vueDevTools({
+        enabled: env.VITE_SHOW_DEVTOOLS === 'true'
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
+    server: {
+      port: 5173,
+      host: true
+    },
+    build: {
+      sourcemap: env.VITE_DEBUG_MODE === 'true'
+    }
+  }
 })
